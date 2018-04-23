c3fb4ee3de6a374f52de3d9845a5766c01f50090
Sam Squires <sam@fawkes.home>
2018-04-23 15:43:03 -0700

    updated flow of slide 1.1-1.6

--- public/js/slide1.js
+++ public/js/slide1.js
@@ -1,13 +1,34 @@
 function slide1() {
     console.log("Displaying Slide 1.1");
-    var placeholder = $("<img id='placeholder' width='100%'>");
-    placeholder.attr("src", "slide1_1.png");
-    $("#graphic").append(placeholder);
+    placeholderImage("slide1_1.png");
 }
 
 function slide1_2() {
     console.log("Displaying Slide 1.2");
+    placeholderImage("slide1_2.png");
+}
+
+function slide1_3() {
+    console.log("Displaying Slide 1.3");
+    placeholderImage("slide1_3.png");
+}
+
+function slide1_4() {
+    console.log("Displaying Slide 1.4");
+    placeholderImage("slide1_4.png");
+}
+
+function slide1_5() {
+    console.log("Displaying Slide 1.5");
+    placeholderImage("slide1_5.png");
+}
+
+function slide1_6() {
+    console.log("Displaying Slide 1.6");
+}
+
+function placeholderImage(imgName) {
     var placeholder = $("<img id='placeholder' width='100%'>");
-    placeholder.attr("src", "slide1_2.png");
+    placeholder.attr("src", imgName);
     $("#graphic").append(placeholder);
 }
